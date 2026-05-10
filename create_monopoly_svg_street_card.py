51d8de6827b4f5fa0595224e7622436f385b6696
Sebastian Haas <sebastian.haas0@googlemail.com>
2026-05-10 16:42:22 +0200

    moved street cards front to jinja

--- create_monopoly_svg_street_card.py
+++ create_monopoly_svg_street_card.py
@@ -12,6 +12,62 @@ card_width = 66.6
 card_height = 76
 
 def create_street_card_frontside(width, height, color, title, price, rents):
+    # 1. Setup Jinja2 (sucht im Ordner 'templates')
+    env = Environment(loader=FileSystemLoader('templates'))
+    template = env.get_template('street_card_front.svg.j2')
+
+    margin = convert.millimeter_to_pixel(3, DPI) # Abstand zum Kartenrand
+    padding = 8 # Lücke zwischen Rahmen und Farbfeld
+    
+    # Berechnungen für das schwebende Farbfeld
+    width_dpi = convert.millimeter_to_pixel(width, DPI)
+    height_dpi = convert.millimeter_to_pixel(height, DPI)
+    header_h = height_dpi * 0.20 # Etwas kleiner für den schwebenden Look
+    rect_x = margin + padding
+    rect_y = margin + padding
+    rect_w = width_dpi - 2 * (margin + padding)
+    # Titel-Position anpassen (Mitte des schwebenden Feldes)
+    title_y = rect_y + (header_h / 2) + 6
+
+    # 3. Daten an das Template übergeben
+    return template.render(
+        width=width_dpi,
+        height=height_dpi,
+        color=color,
+        title_size=22,
+        font_size=12,
+        font_b64=read.as_base64("font/MONOPOLY_INLINE.woff2"),
+        img_b64=read.as_base64("img/figure1.svg"),
+        title=title,
+        price=price,
+        rents=rents,
+        margin=margin,
+        title_y=title_y,
+        rect_x=rect_x,
+        rect_y=rect_y,
+        rect_w=rect_w,
+        header_h=header_h,
+        inner_width = (width_dpi - 2*margin),
+        inner_height = (height_dpi - 2*margin),
+    )
+
+def create_street_card_backside(width, height, title, price, rents):
+    # 1. Setup Jinja2 (sucht im Ordner 'templates')
+    env = Environment(loader=FileSystemLoader('templates'))
+    template = env.get_template('community_card_back.svg.j2')
+
+    # 3. Daten an das Template übergeben
+    return template.render(
+        width=convert.millimeter_to_pixel(width, DPI),
+        height=convert.millimeter_to_pixel(height, DPI),
+        color=activity_card_color,
+        title_size=32,
+        font_size=12,
+        font_b64=read.as_base64("font/MONOPOLY_INLINE.woff2"),
+        img_b64=read.as_base64("img/figure1.svg"), # TODO: find tressure box as SVG, as grey background
+    )
+
+def create_street_card_frontside_bak(width, height, color, title, price, rents):
     header_h = height * 0.20 # Etwas kleiner für den schwebenden Look
     margin = convert.millimeter_to_pixel(3, DPI) # Abstand zum Kartenrand
     padding = 8            # Lücke zwischen Rahmen und Farbfeld
@@ -92,7 +148,7 @@ def create_street_card_frontside(width, height, color, title, price, rents):
 </svg>'''
     return svg
 
-def create_street_card_backside(width, height, title, price, rents):
+def create_street_card_backside_bak(width, height, title, price, rents):
     margin = 12              # Abstand zum Kartenrand
     padding = 0          # Lücke zwischen Rahmen und Farbfeld
     color = "#FF0000"
@@ -166,13 +222,14 @@ def generate_from_csv(csv_filename):
             # Mieten aus CSV sammeln
             mieten = [
                 row['miete_basis'], row['miete_1h'], row['miete_2h'], 
-                row['miete_3h'], row['miete_4h'], row['miete_hotel']
+                row['miete_3h'], row['miete_4h'], row['miete_hotel'],
+                row['kosten_haus'], row['kosten_hotel']
             ]
             
             # Karte generieren (mit deiner Original-Logik)
             svg_content = create_street_card_frontside(
-                width=convert.millimeter_to_pixel(card_width, DPI), 
-                height=convert.millimeter_to_pixel(card_height, DPI),
+                width=card_width, 
+                height=card_height,
                 color=row['farbe'], 
                 title=row['name'], 
                 price=int(row['preis']), 
@@ -185,18 +242,18 @@ def generate_from_csv(csv_filename):
             print(f"file {filename} created")
 
             # Karte generieren (mit deiner Original-Logik)
-            svg_content = create_street_card_backside(
-                width=convert.millimeter_to_pixel(card_width, DPI), 
-                height=convert.millimeter_to_pixel(card_height, DPI),
-                title=row['name'], 
-                price=int(row['preis']), 
-                rents=mieten
-            )
-        
-            filename = f"output/streets/{row['name'].replace(' ', '_')}_back.svg"
-            with open(filename, "w", encoding="utf-8") as f:
-                f.write(svg_content)
-            print(f"file {filename} created")
+            #svg_content = create_street_card_backside(
+            #    width=convert.millimeter_to_pixel(card_width, DPI), 
+            #    height=convert.millimeter_to_pixel(card_height, DPI),
+            #    title=row['name'], 
+            #    price=int(row['preis']), 
+            #    rents=mieten
+            #)
+        #
+            #filename = f"output/streets/{row['name'].replace(' ', '_')}_back.svg"
+            #with open(filename, "w", encoding="utf-8") as f:
+            #    f.write(svg_content)
+            #print(f"file {filename} created")
 
 if __name__ == "__main__":
     # Falls du die CSV noch nicht hast, erstelle sie kurz mit den Spaltennamen:
